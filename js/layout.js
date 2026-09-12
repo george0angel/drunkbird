ddcab5bdb25f7962383f5e394f083977c7835566
George Angel <62700399+george0angel@users.noreply.github.com>
2026-09-12 18:21:47 +0100

    Fix layout asset URLs

--- js/layout.js
+++ js/layout.js
@@ -2,7 +2,11 @@ import headHtml from "/content/layout/head.html?raw";
 import headerHtml from "/content/layout/header.html?raw";
 import footerHtml from "/content/layout/footer.html?raw";
 
+import faviconUrl from "/assets/favicon.svg?url";
+import githubIconUrl from "/assets/GitHub_Invertocat_Black.svg?url";
+
 document.querySelector("#site-head").insertAdjacentHTML("beforeend", headHtml);
+document.querySelector("#site-favicon").href = faviconUrl;
 
 document
   .querySelector("#site-header")
@@ -11,5 +15,6 @@ document
 document
   .querySelector("#site-footer")
   .insertAdjacentHTML("beforeend", footerHtml);
+document.querySelector("#github-icon").src = githubIconUrl;
 
 document.body.style.opacity = 1;
