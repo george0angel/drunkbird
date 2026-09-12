a7f2701bedf57397018e4581814985d6ebb81618
George Angel <62700399+george0angel@users.noreply.github.com>
2026-09-12 13:49:32 +0100

    Separate head, header, and footer from index.html

---
+++ js/layout.js
@@ -0,0 +1,16 @@
+const htmlFiles = import.meta.glob(`/content/layout/*.html`, {
+  query: `?raw`,
+  import: `default`,
+  eager: true,
+});
+
+const folder = `/content/layout/`;
+
+document.querySelector(`#site-head`).innerHTML +=
+  htmlFiles[`${folder}head.html`];
+
+document.querySelector(`#site-header`).innerHTML +=
+  htmlFiles[`${folder}header.html`];
+
+document.querySelector(`#site-footer`).innerHTML +=
+  htmlFiles[`${folder}footer.html`];
